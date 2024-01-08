61e062f1d7d010be023abb59c9562ccc4483e0fe
kristrushh <kristrush@yandex.ru>
2024-01-08 20:02:14 +0300

    add lesson-01/06

--- lesson-01/06.js
+++ lesson-01/06.js
@@ -1,14 +1,4 @@
-let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
+let str = "zzzzyzz"
+str.includes("y") ? console.log("нашел") : console.log("заблудился")
 
-switch (true) {
-    case rule.length < 25:
-        console.log("Все таки нет правил без исключения");
-        break;
-    case rule.length > 25:
-        console.log("У меня 100 пудов все получится");
-        break;
-    case rule.length === 25:
-        console.log("50 на 50");
-        break;
-}
 
