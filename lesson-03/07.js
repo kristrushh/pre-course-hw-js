b38736838497dd3697a27ab10479553a43c58c69
kristrushh <kristrush@yandex.ru>
2024-03-01 22:38:33 +0300

    Filter

--- lesson-03/07.js
+++ lesson-03/07.js
@@ -3,7 +3,6 @@ const numbers = [
     [11, 20, 33, 40, 55],
     [111, 200, 333, 400, 555],
 ];
-
 for (let i = 0; i < numbers.length; i++) {
     numbers[i] = numbers[i].filter((n) => n % 2 === 0)
 }
